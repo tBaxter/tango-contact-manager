22376252c3297efa099b2845d28e0d2779bad650
Tim Baxter <tim.baxter@cerner.com>
2013-08-23 16:24:21 -0500

    Initial commit of tango-contact-manager
    Extracted from Tango

---
+++ README.md
@@ -0,0 +1,13 @@
+Tango Contact Manager
+=====
+
+Provides contact forms and any other sort of user submission form you might want. You can create new forms on the fly, straight from the admin.
+
+##Installation:
+
+    pip install git+https://github.com/tBaxter/tango-contact-manager.git
+
+
+## Usage:
+Add 'contact_manager' to your installed apps, then run syncdb or migrate.
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='tango-contact-manager',
+    version='0.3',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    url='https://github.com/tBaxter/tango-contact-manager',
+    license='LICENSE',
+    description="""Provides contact forms and any other user submission form you might want. 
+        Create user submission forms on the fly, straight from the Django admin.
+        """,
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True
+)
