caedceb9c2074198446e484dc27eeedf2df05cd3
Tim Baxter <tim.baxter@cerner.com>
2014-12-23 08:31:43 -0600

    classifiers

--- setup.py
+++ setup.py
@@ -15,5 +15,15 @@ setup(
     long_description=open('README.md').read(),
     packages=find_packages(),
     zip_safe=False,
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
